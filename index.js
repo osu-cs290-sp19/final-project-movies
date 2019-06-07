d458858f7e2c572fe98d8d36cc20e9d4eac4265b
Margaret Walters <waltmarg@flip2.engr.oregonstate.edu>
2019-06-07 10:28:16 -0700

    create post modal now working!

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-var modal = document.getElementById('create-twit-modal');
+
+/************************************************************/
+/*First step: making the modal show up when you click button*/
+/************************************************************/
+var createPostButton = document.getElementById('add-post-button');
+var modal = document.getElementById('create-post-modal');
+var modalBackground = document.getElementById('modal-backdrop');
+
+createPostButton.addEventListener("click", function(event){
+  console.log("create post button was clicked", event.target)
+
+  //what to do once it is clicked
+  //remove the hidden class
+  modal.classList.remove('hidden');
+  modalBackground.classList.remove('hidden');
+})
+
+/*************************************************************/
+/*Second step: make sure you can cancel out of modal*/
+/*************************************************************/
+var insertText = document.getElementById('post-text-input');
+var attributionText = document.getElementById('post-attribution-input');
+var urlInput = document.getElementById('post-url-input');
+
+var cancelButton = document.getElementsByClassName('modal-cancel-button');
+cancelButton[0].addEventListener("click", function(event){
+  console.log("the cancel button was clicked", event.target);
+
+  //hide the modal if the user cancels there thing
+  modal.classList.add('hidden');
+  modalBackground.classList.add('hidden');
+
+  //make sure that the input fields are cleared.
+  attributionText.value = "";
+  insertText.value = "";
+  urlInput.value = "";
+})
+
+var xButton = document.getElementsByClassName('modal-close-button');
+xButton[0].addEventListener("click", function(event){
+  console.log("the close button was clicked", event.target);
+
+  //hide the modal if the user cancels there thing
+  modal.classList.add('hidden');
+  modalBackground.classList.add('hidden');
+
+  //make sure that the input fields are cleared.
+  attributionText.value = "";
+  insertText.value = "";
+  urlInput.value = "";
+})
+
+/*************************************************************/
+/*step 3: accept button working, place movie in container.****/
+/*make sure all required fields have input********************/
+/*************************************************************/
+
+
+/*var modal = document.getElementById('create-twit-modal');
 
 var background = document.getElementById('modal-backdrop');
 
@@ -15,8 +73,9 @@ var contribute = document.getElementsByClassName('modal-accept-button');
 var input = document.getElementsByClassName('post-input-element');
 
 var post = document.getElementsByClassName('post')[0];
+*/
 
-btn.addEventListener('click', function(event) {
+/*btn.addEventListener('click', function(event) {
   modal.style.display = "block";
   background.style.display = "none";
   background.removeClass("hidden");
@@ -141,3 +200,4 @@ searchBar.addEventListener('change', function(event){
 	console.log(searching);
 	wordSearched(searching);
 });
+*/
