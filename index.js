ddaa7360bcf44bf37cc6b6da8286ef870717ff19
Nora Quick <quickn@oregonstate.edu>
2019-06-08 10:26:19 -0700

    states and dice

--- index.js
+++ index.js
@@ -194,6 +194,28 @@ function setRating(){
   });
 }
 
+var dice = {
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+//Prints dice roll to the page
+
+function printNumber(number) {
+  var placeholder = document.getElementById('placeholder');
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+
 /**************************************************************/
 /*IMPLEMENT THE SEARCH BAR*/
 /**************************************************************/
