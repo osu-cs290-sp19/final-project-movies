1bfb32ef4bbd099a9c515a6b8303bb43d65d3d5c
Nora Quick <quickn@oregonstate.edu>
2019-06-10 11:55:30 -0700

    .

--- server.js
+++ server.js
@@ -4,37 +4,36 @@
  * names: Margaret Walters & Nora Quick
  */
 
-var path = require('path');
-var express = require('express');
-var exphandle = require('express-handlebars');
+ var path = require('path');
+ var express = require('express');
+ var exphandle = require('express-handlebars');
 
-//var postdata = require('./postData');
+ //var postdata = require('./postData');
 
-var app = express();
-var port = process.env.PORT || 3305;
+ var app = express();
+ var port = process.env.PORT || 3305;
 
-app.engine('handlebars', exphandle({ defaultLayout: '/views/main'}));
-app.set('view engine', 'handlebars');
+ app.engine('handlebars', exphandle({ defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
+ app.use(express.static('public'));
 
-app.get('/', function (req, res, next){
-  res.status(200).render('/views/main');
-});
+ app.get('/', function (req, res, next){
+   res.status(200).render('multPosts', {post:postData, dispMod:false});
+ });
 
-app.get('/posts/:genre', function(req, res, next){
-  var genre = req.params.genre.toLowerCase();
-  if(postData[req.genre]){
-    res.status(200).render('multPosts', {post:[postData[req.params.genre]], desplayMod: true});
-  } else {
-    res.status(404).render('404');
-  }
-});
+ app.get('/posts/:genre', function(req, res, next){
+   if(postData[req.params.genre]){
+     res.status(200).render('multPosts', {post:[postData[req.params.genre]], desplayMod: true});
+   } else {
+     res.status(404).render('404');
+   }
+ });
 
-app.get('*', function (req, res, next){
-  res.status(404).render('404');
-});
+ app.get('*', function (req, res, next){
+   res.status(404).render('404');
+ });
 
-app.listen(port, function (){
-  console.log("== Server is listening to port", port);
-});
+ app.listen(port, function (){
+   console.log("== Server is listening to port", port);
+ });
